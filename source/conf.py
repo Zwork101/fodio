c42c857d2ca5c05d34e0d4e05ff6133817b53ee3
Zwork101 <zwork101@gmail.com>
2018-07-17 17:42:23 -0400

    Whoops, that's why autodoc wasn't working! (part 4)
    Signed-off-by: Zwork101 <zwork101@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath("."))
+_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, _path)
 
 
 # -- Project information -----------------------------------------------------
